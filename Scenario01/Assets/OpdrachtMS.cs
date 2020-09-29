c94e008dea63d975e8b98f744160aed8f3bc35cf
Nickvdmullen <Nickvdmullen@gmail.com>
2020-09-29 13:13:52 +0200

    Revert "Alpha"
    This reverts commit 631aa4c7e87c8f1a7882d5d3d6a993241e23659e.

---
+++ Scenario01/Assets/OpdrachtMS.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpdrachtMS : MonoBehaviour
+{
+    public GameObject opdracht1;
+    public GameObject opdracht2;
+    public GameObject opdracht3;
+    public GameObject opdracht4;
+    public GameObject opdracht5;
+
+    public int status = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (status)
+        {
+            case 0:
+                opdracht1.SetActive(false);
+                opdracht2.SetActive(false);
+                opdracht3.SetActive(false);
+                opdracht4.SetActive(false);
+                opdracht5.SetActive(false);
+                break;
+            case 1:
+                opdracht1.SetActive(true);
+                opdracht2.SetActive(false);
+                opdracht3.SetActive(false);
+                opdracht4.SetActive(false);
+                opdracht5.SetActive(false);
+                break;
+            case 2:
+                opdracht1.SetActive(false);
+                opdracht2.SetActive(true);
+                opdracht3.SetActive(false);
+                opdracht4.SetActive(false);
+                opdracht5.SetActive(false);
+                break;
+            case 3:
+                opdracht1.SetActive(false);
+                opdracht2.SetActive(false);
+                opdracht3.SetActive(true);
+                opdracht4.SetActive(false);
+                opdracht5.SetActive(false);
+                break;
+            case 4:
+                opdracht1.SetActive(false);
+                opdracht2.SetActive(false);
+                opdracht3.SetActive(false);
+                opdracht4.SetActive(true);
+                opdracht5.SetActive(false);
+                break;
+            case 5:
+                opdracht1.SetActive(false);
+                opdracht2.SetActive(false);
+                opdracht3.SetActive(false);
+                opdracht4.SetActive(false);
+                opdracht5.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+}
