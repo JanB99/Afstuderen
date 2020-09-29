677aae54c002c6e78febb0e550c58898e289ffb3
Nickvdmullen <Nickvdmullen@gmail.com>
2020-09-29 13:16:30 +0200

    Revert "Revert "Alpha""
    This reverts commit c94e008dea63d975e8b98f744160aed8f3bc35cf.

---
+++ Scenario01/Assets/SymptoomScripts/Blaas.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blaas : MonoBehaviour
+{
+
+    public int BlaasIncrease = 10;
+    public int MaxBlaasVolume = 1000;
+    public PlayerController playerController;
+    public Text Text;
+    public GameObject panel;
+    public GameObject Image;
+    public AudioClip currentClip;
+    public AudioSource choice;
+
+    private bool SafeSpace;
+    public int CurrentBlaasVolume = 0;
+
+    void Start()
+    {
+        InvokeRepeating("EverySecond",0,1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        SafeSpace = playerController.safeSpace;
+    }
+
+    private void EverySecond()
+    {
+        if(!SafeSpace)
+        {
+            if(CurrentBlaasVolume < MaxBlaasVolume)
+            {
+                if(CurrentBlaasVolume > 100 && CurrentBlaasVolume < 300)
+                {
+                    CurrentBlaasVolume += BlaasIncrease;
+                     if(!choice.isPlaying)
+                        {
+                        choice.clip = currentClip;
+                        choice.Play();
+                        }
+                }
+                else if(CurrentBlaasVolume > 300 && CurrentBlaasVolume < 600)
+                {
+                    
+                    Image.gameObject.SetActive(true);
+                    choice.volume = 0.66f;
+                    CurrentBlaasVolume += BlaasIncrease;
+                }   
+                else if(CurrentBlaasVolume > 600 && CurrentBlaasVolume < 900)
+                {
+                    panel.gameObject.SetActive(true);
+                    Text.gameObject.SetActive(true);
+                    choice.volume = 1.0f;
+                    Text.text = "Ik kan het bijna niet meer ophouden. Ik moet naar de WC.";
+                    CurrentBlaasVolume += BlaasIncrease;
+                }
+                else
+                {
+                    CurrentBlaasVolume += BlaasIncrease;
+                }
+
+                if(CurrentBlaasVolume > MaxBlaasVolume)
+                {
+                    Text.text = "Kon het niet meer ophouden. Ik heb in mijn broek geplast.";
+                    playerController.speed = 0.8f;
+                }
+                
+            }
+        }
+        else
+        {
+            Text.gameObject.SetActive(false);
+            Image.gameObject.SetActive(false);
+            panel.gameObject.SetActive(false);
+            choice.Stop();
+            choice.volume = 0.33f;
+            CurrentBlaasVolume = 0;
+            playerController.speed = 1;
+        }
+
+    }
+
+
+}
---
+++ Scenario01/Assets/opdracht1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opdracht1 : MonoBehaviour
+{
+
+    public int numberinzone = 0;
+    public OpdrachtMS Opdrachten;
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if(numberinzone == 5)
+        {
+            if(Opdrachten.status == 1)
+            {
+            Opdrachten.status = 2;
+            }
+        }
+    }
+
+   void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Empty")
+        {
+            numberinzone++;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag =="Empty")
+        {
+            numberinzone--;
+        }
+    }
+}
